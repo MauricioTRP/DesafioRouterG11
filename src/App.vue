61e3fcc9e4bb17a7c407c783bbf51d69bfee25f7
Mauricio Fuentes <mauriciofb@gmail.com>
2024-08-18 17:20:56 -0400

    (feature): Base desafio Router Vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+import MediosPago from './components/MediosPago.vue'
+</script>
+
+<template>
+  <Navbar />
+  <RouterView />
+  <MediosPago />
+  <Footer />
+</template>
+
+<style scoped></style>
